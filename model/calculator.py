55b9da4e6802ab330063866f3af87101299eda3d
NikolNat <102613704+NikolNat@users.noreply.github.com>
2022-12-06 20:37:49 +0300

    use unit test on DYNAMIC calculator with Class fixture method SetUpClass and tearDownClass

--- model/calculator.py
+++ model/calculator.py
@@ -2,20 +2,15 @@ class Calculator:
     def __init__(self, a=0, b=0):
         self.a = a
         self.b = b
-        self.count = 0
 
     def sum(self):
-        self.count += 1
         return self.a + self.b
 
     def sub(self):
-        self.count += 1
         return self.a - self.b
 
     def mul(self):
-        self.count += 1
         return self.a * self.b
 
     def div(self):
-        self.count += 1
         return self.a // self.b
--- test/unit_tests/test_calculator.py
+++ test/unit_tests/test_calculator.py
@@ -4,61 +4,62 @@ from model.calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
-    def setUp(self):
-        self.calc = Calculator()
+    calc = Calculator()
 
-    def tearDown(self):
-        del self.calc
+    @classmethod
+    def setUpClass(cls):
+        cls.calc = Calculator()
+        # TestCalculator.calc = Calculator()
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.calc
 
     # AAA
     def test_sum(self):
         # arrange
-        self.calc.a = 10
-        self.calc.b = 20
+        TestCalculator.calc.a = 10
+        TestCalculator.calc.b = 20
         expected = 30
 
         # action
-        actual = self.calc.sum()
+        actual = TestCalculator.calc.sum()
 
         # assert
         self.assertEqual(expected, actual)
-        self.assertEqual(1, self.calc.count)
 
     def test_sub(self):
-        self.calc.a = 10
-        self.calc.b = 7
+        TestCalculator.calc.a = 10
+        TestCalculator.calc.b = 7
         expected = 3
 
-        actual = self.calc.sub()
+        actual = TestCalculator.calc.sub()
 
         self.assertEqual(expected, actual)
-        self.assertEqual(1, self.calc.count)
 
     def test_mul(self):
         # arrange
-        self.calc.a = 8
-        self.calc.b = 7
+        TestCalculator.calc.a = 8
+        TestCalculator.calc.b = 7
         expected = 56
 
         # action
-        actual = self.calc.mul()
+        actual = TestCalculator.calc.mul()
 
         # assert
         self.assertEqual(expected, actual)
-        self.assertEqual(1, self.calc.count)
 
     def test_div(self):
         # arrange
-        self.calc.a = 18
-        self.calc.b = 7
+        TestCalculator.calc.a = 18
+        TestCalculator.calc.b = 7
         expected = 2
 
         # action
-        actual = self.calc.div()
+        actual = TestCalculator.calc.div()
 
         # assert
         self.assertEqual(expected, actual)
-        self.assertEqual(1, self.calc.count)
 
 
 if __name__ == "__main__":
